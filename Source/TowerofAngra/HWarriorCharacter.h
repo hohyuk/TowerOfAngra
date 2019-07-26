35266fe59602cef2d2abb2700b15c58699276379
hohyuk <ghgur91@naver.com>
2019-07-26 12:11:45 +0900

    새캐릭터

---
+++ Source/TowerofAngra/HWarriorCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TowerofAngraCharacter.h"
+#include "HWarriorCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWEROFANGRA_API AHWarriorCharacter : public ATowerofAngraCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
